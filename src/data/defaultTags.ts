029f3d48f8bcfcb5f21169cf09ace0dfe069a017
Zubair Ahmed <zubair1024@gmail.com>
2023-02-05 15:47:13 +0400

    changing tag

--- src/data/defaultTags.ts
+++ src/data/defaultTags.ts
@@ -3,9 +3,9 @@ import { IDefaultTags } from '@/util/types';
 const defaultTags: IDefaultTags = {
   title: 'Tree Visualizer',
   description: 'Simple Tree visualizer built by zubair',
-  image: 'https://www.grizzlybit.dev/grizzlybit-logo-image.png',
+  image: '/grizzlybit-logo-image.png',
   imageType: 'image/png',
-  websiteUrl: 'https://www.grizzlybit.dev',
+  websiteUrl: 'https://www.google.com',
   twitterHandle: '@zubair1024',
 };
 
